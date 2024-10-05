5e8b9a55a4bba1c4de59318a0affb588754057be
Kevin Eduardo <ke47769@gmail.com>
2024-10-05 18:59:37 -0300

    Add files via upload
    Calculo para saber se o usuário é maior do idade ou menor de idade.

---
+++ CalculoMaioridade.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CalculoMaioriadade;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Digite seu nome;");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Olá {name}! ");
+        Console.Write("Digite o ano do seu nascimento: ");
+        int year = int.Parse(Console.ReadLine());
+        int age = 2024 - year; //+,-,*,/
+        Console.WriteLine($"Voce tem {age} anos.");
+
+        if (age >= 18)
+        {
+            Console.WriteLine("Voce é maior de idade. ");
+        }
+        else
+        {
+            Console.WriteLine("Voce é menor de idade. ");
+        }
+    }
+}
+
