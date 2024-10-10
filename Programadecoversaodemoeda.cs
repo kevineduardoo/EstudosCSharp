dea0fc8da65c522d9badbd4118aab0dd18b48eea
Kevin Eduardo <ke47769@gmail.com>
2024-10-10 19:17:19 -0300

    Add files via upload
    Aprendizado sobre interpolação de strings em C#
    Aplicado em um conversor de moeda simples(de dólar em real)

---
+++ Programadecoversaodemoeda.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Coversordemoeda;
+
+class Programadecoversaodemoeda
+{
+    static void Main(string[] args)
+    {
+        string ValorDigitadoEmReal = "";
+        decimal ValorConvertido;
+        decimal ValorEmDolar;
+        decimal CotacaoDoDolarHoje = 5.58M;
+        Console.WriteLine("Digite o valor que deseja converter: ");
+
+        ValorDigitadoEmReal = Console.ReadLine();
+        if (!Decimal.TryParse(ValorDigitadoEmReal, out ValorConvertido))
+        {
+            Console.WriteLine("Digite um valor válido. Não foi possível converter '{0}'.", ValorDigitadoEmReal);
+        }
+
+        ValorEmDolar = ValorConvertido * CotacaoDoDolarHoje;
+
+        Console.WriteLine("Valor R$ {0} convertido em dolar é: R$ {1}", ValorDigitadoEmReal, ValorEmDolar);
+    }
+
+
+}
