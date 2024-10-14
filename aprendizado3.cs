a211b14e1240b2a1bfdef06281a3e8e2fd054726
Kevin Eduardo <ke47769@gmail.com>
2024-10-14 19:42:29 -0300

    Add files via upload
    Estudos sobre loops parte 3 no C#

---
+++ aprendizado3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace aprendizado3;
+
+class Program
+{
+    static void Main()
+    {
+        string[] names = { "Kevin", "Stéfanie", "Luka", "Pudim" };  // Syntactic sugar(basicamente e uma funcionabilidade no compilador q permite q a gente digite menos codigos pra certas 
+
+        foreach (string name in names)
+        {
+            Console.WriteLine(name);
+        }
+
+    }
+
+}
+
+
