d8e8cb5ae562e858562120a5ea2ba1b67decbe68
Kevin Eduardo <ke47769@gmail.com>
2024-10-14 19:41:26 -0300

    Add files via upload
    Estudos sobre loops no C#

---
+++ aprendizado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Aprendizado;
+
+class Usandofor
+{
+    static void Main()
+    {
+        string[] names = { "Kevin", "Stéfanie", "Luka", "Pudim" };  // Syntactic sugar(basicamente e uma funcionabilidade no compilador q permite q a gente digite menos codigos pra certas 
+
+        for (int i = 0; i < 4; i++)
+        {
+            Console.WriteLine(names[i]);
+        }
+
+    }
+
+}
+
+
