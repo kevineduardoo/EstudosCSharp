421ff8b1a671001a632d74fe26fa4142473b98ef
Kevin Eduardo <ke47769@gmail.com>
2024-10-16 21:04:10 -0300

    Add files via upload
    Aprendizado sobre(Casesensitive,Equals,StringComparison e OrdinalIgnoreCase 
    Em C#

---
+++ aprendizado4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aprendizado4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Kevin", "Stéfanie", "Luka", "Pudim" };
+
+            // Usando string.Equals para comparar ignorando diferença entre maiúsculas e minúsculas
+            if (string.Equals(names[0], "kevin", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Igual");
+            }
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
